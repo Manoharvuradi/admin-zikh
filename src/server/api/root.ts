2e830863ec31b1dd5cdbe4f247c569dc81464176
Manoharvuradi <mvuradi@linkfields.com>
2024-02-24 19:56:41 +0530

    added crime tip api

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { searchByCityRouter } from "./routers/searchByCity";
 import { searchByStateRouter } from "./routers/searchByState";
+import { crimeTipRouter } from "./routers/crimetip";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { searchByStateRouter } from "./routers/searchByState";
 export const appRouter = createTRPCRouter({
   searchByCity: searchByCityRouter,
   searchByState: searchByStateRouter,
+  crimeTip: crimeTipRouter,
 });
 
 // export type definition of API
